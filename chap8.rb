99d11aad0e8ef78f2050ba58df1694684a8ed489
Carlos <copez.larlos@gmail.com>
2015-07-19 23:00:43 -0700

    Initial commit + 1 file

---
+++ chap8.rb
@@ -0,0 +1,5 @@
+puts 'Hello, and thank you for taking the time to'
+puts 'help me with this experiment. My experiment'
+puts 'has to do with the way people feel about'
+puts 'Mexican food. Just think about Mexican food'
+puts 'and try '
