0bcca6a5df7c77b5ef926c5eb17e1863bc22b558
Carlos <copez.larlos@gmail.com>
2015-07-21 15:49:22 -0700

    Completed 8_2 and 8_3

---
+++ chap8_2.rb
@@ -0,0 +1,17 @@
+def sayMoo
+  puts 'Moooooo...'
+end
+
+sayMoo
+sayMoo
+puts 'coin-coin'
+sayMoo
+sayMoo
+
+def sayMoo numberOfMoos
+  puts 'mooooooo...' * numberOfMoos
+end
+
+sayMoo 3
+puts 'oink-oink'
+sayMoo
